94bf26072efae26edfdf1b2307e9a70765fd3c92
Oscar Leal <toshiro120000@gmail.com>
2023-08-15 21:13:27 -0400

    espacio

--- procesar_registro.php
+++ procesar_registro.php
@@ -6,6 +6,7 @@ $username = "root"; // Cambia esto por tu nombre de usuario de MySQL
 $password = ""; // Cambia esto por tu contraseña de MySQL
 $dbname = "evaluador_salud"; // Cambia esto por el nombre de tu base de datos
 
+
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
