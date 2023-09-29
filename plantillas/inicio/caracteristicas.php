58e49abb970738f59dc0b5c71ccfa1584a36d0a3
Oscar Leal <toshiro120000@gmail.com>
2023-09-29 13:25:12 -0300

    cambios pequeños

--- plantillas/inicio/caracteristicas.php
+++ plantillas/inicio/caracteristicas.php
@@ -25,26 +25,28 @@
                     <p>Compara diferentes escenarios de salud para tomar decisiones informadas sobre tu bienestar. Puedes ver cómo cambiaría tu vida en el futuro.</p>
                 </div>
             </div>
-            <!-- Agrega más características aquí -->
+            <!-- Característica 4 -->
             <div class="col-md-4">
                 <div class="feature">
                     <h3>Generación de Recomendaciones</h3>
                     <p>Basándonos en los resultados del análisis, te ofrecemos recomendaciones específicas para mejorar tu salud. Desde cambios en la dieta hasta rutinas de ejercicio.</p>
                 </div>
             </div>
+            <!-- Característica 5 -->
             <div class="col-md-4">
                 <div class="feature">
                     <h3>Asistente Virtual Interactivo</h3>
                     <p>Nuestro asistente virtual te guía a través de tus resultados, responde tus preguntas y te brinda apoyo en tu viaje hacia un mejor bienestar.</p>
                 </div>
             </div>
+            <!-- Característica 6 -->
             <div class="col-md-4">
                 <div class="feature">
                     <h3>Seguimiento Personalizado</h3>
                     <p>Te ayudamos a llevar un seguimiento de tu progreso y a establecer metas de salud personalizadas. Controla tu estado de salud de manera efectiva.</p>
                 </div>
             </div>
-            <!-- Agrega más características según sea necesario -->
+            <!-- Agregar más características según sea necesario -->
         </div>
     </div>
 </section>
--- plantillas/inicio/header.php
+++ plantillas/inicio/header.php
@@ -5,38 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Evaluador de Salud Personalizado</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css"  href="/capston/assets/estilo.css?ver=2" >
   </head> 
-  <body>
+<body>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Health Future</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/capston/plantillas/inicio/index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/capston/plantillas/inicio/caracteristicas.php">Características</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/capston/plantillas/evaluador_de_salud/evaluador.php">Evaluador(test)</a>
-        </li>
-      </ul>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle me-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Usuario
-            </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/capston/plantillas/login/login.php">Ingresa</a></li>
-            <li><a class="dropdown-item" href="/capston/plantillas/registro/registro.php">Registro</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Cerrar sesion</a></li>
-          </ul>
-        </li>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Health Future</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/capston/plantillas/inicio/index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/capston/plantillas/inicio/caracteristicas.php">Características</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/capston/plantillas/evaluador_de_salud/evaluador.php">Evaluador(test)</a>
+          </li>
+        </ul>
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle me-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Usuario
+              </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="/capston/plantillas/login/login.php">Ingresa</a></li>
+              <li><a class="dropdown-item" href="/capston/plantillas/registro/registro.php">Registro</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Cerrar sesion</a></li>
+            </ul>
+          </li>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
