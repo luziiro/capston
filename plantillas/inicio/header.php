f36d4df2718f455ecb360c8c91b101961a3f2f37
Jonathan Bustamante <82405395+hasek2@users.noreply.github.com>
2023-10-19 21:30:44 -0300

    s

--- plantillas/inicio/header.php
+++ plantillas/inicio/header.php
@@ -1,65 +1,59 @@
-<!DOCTYPE html><link rel="stylesheet" type="text/css" href="estilo-fondo.css">
-
+<!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Evaluador de Salud Personalizado</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css"  href="/capston/assets/estilo.css?ver=2" >
-  </head> 
+    <!-- Agregamos los enlaces a Bootstrap y otros estilos -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoI... crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/capston/assets/estilo.css?ver=2">
+    <!-- Agregamos un enlace a tu archivo de estilo personalizado (si lo tienes) -->
+    <link rel="stylesheet" type="text/css" href="estilo-fondo.css">
+</head>
 <body>
-
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/capston/plantillas/inicio/index.php">Health Future</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/capston/plantillas/inicio/index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/capston/plantillas/inicio/caracteristicas.php">Características</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/capston/plantillas/evaluador_de_salud/evaluador.php">Evaluador(test)</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/capston/plantillas/evaluador_de_salud/evaluador_home.php">Evaluador home(test(test))</a>
-        </li>
-      </ul>
-      <ul class="navbar-nav">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle me-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <?php
-            session_start();
-            if (isset($_SESSION['usuario_logueado']) && $_SESSION['usuario_logueado']) {
-                // El usuario está logeado, muestra el nombre del usuario
-                echo $_SESSION["nombre_usuario"];
-            } else {
-                // El usuario no está logeado, muestra "Usuario"
-                echo 'Usuario';
-            }
-            ?>
-          </a>
-          <ul class="dropdown-menu">
-            <?php
-            if (isset($_SESSION['usuario_logueado']) && $_SESSION['usuario_logueado']) {
-                // El usuario está logeado, muestra solo "Cerrar sesión"
-                echo '<li><a class="dropdown-item" href="/capston/plantillas/login/procesar_logout.php">Cerrar sesión</a></li>';
-            } else {
-                // El usuario no está logeado, muestra "Ingresa" y "Registro"
-                echo '<li><a class="dropdown-item" href="/capston/plantillas/login/login.php">Ingresa</a></li>';
-                echo '<li><a class="dropdown-item" href="/capston/plantillas/registro/registro.php">Registro</a></li>';
-            }
-            ?>
-            <li><hr class="dropdown-divider"></li>
-          </ul>
-        </li>
-      </ul>
+    <div class="container">
+        <a class="navbar-brand" href="/capston/plantillas/inicio/index.php">Health Future</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/capston/plantillas/inicio/index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/capston/plantillas/inicio/caracteristicas.php">Características</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/capston/plantillas/evaluador_de_salud/evaluador.php">Evaluador(test)</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle me-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <?php
+                        session_start();
+                        if (isset($_SESSION['usuario_logueado']) && $_SESSION['usuario_logueado']) {
+                            echo $_SESSION["nombre_usuario"];
+                        } else {
+                            echo 'Usuario';
+                        }
+                        ?>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <?php
+                        if (isset($_SESSION['usuario_logueado']) && $_SESSION['usuario_logueado']) {
+                            echo '<li><a class="dropdown-item" href="/capston/plantillas/login/procesar_logout.php">Cerrar sesión</a></li>';
+                        } else {
+                            echo '<li><a class="dropdown-item" href="/capston/plantillas/login/login.php">Ingresa</a></li>';
+                            echo '<li><a class="dropdown-item" href="/capston/plantillas/registro/registro.php">Registro</a></li>';
+                        }
+                        ?>
+                        <li><hr class="dropdown-divider"></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
 </nav>
