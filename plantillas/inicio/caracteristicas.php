da2c608423a6e3c58f1af19cb3f6c6cba1ca02d8
Jonathan Bustamante <82405395+hasek2@users.noreply.github.com>
2023-10-19 21:25:53 -0300

    Revert "diseño"
    This reverts commit 24ff62a8c5b5d5c0f0ac92a8629cc9f0f1f537e6.

--- plantillas/inicio/caracteristicas.php
+++ plantillas/inicio/caracteristicas.php
@@ -1,47 +1,47 @@
 <?php include 'header.php'; ?>
 
-<section class="py-5 bg-light" id="features">
+<section class="py-5" id="features">
     <div class="container">
         <h2 class="text-center cambio-color">Características Clave</h2>
         <div class="row">
             <!-- Característica 1 -->
             <div class="col-md-4">
-                <div class="feature p-4 mb-4 bg-white rounded text-center">
+                <div class="feature">
                     <h3>Análisis Detallado</h3>
                     <p>Utilizamos algoritmos avanzados para evaluar tu estado de salud actual y predecir posibles riesgos. Nuestro análisis incluye datos biométricos, hábitos de vida y más.</p>
                 </div>
             </div>
             <!-- Característica 2 -->
             <div class="col-md-4">
-                <div class="feature p-4 mb-4 bg-white rounded text-center">
+                <div class="feature">
                     <h3>Visualización Interactiva</h3>
                     <p>Explora visualmente cómo podrías lucir y sentirte en el futuro con cambios en tus hábitos. Nuestras visualizaciones te muestran aspectos físicos y emocionales.</p>
                 </div>
             </div>
             <!-- Característica 3 -->
             <div class="col-md-4">
-                <div class="feature p-4 mb-4 bg-white rounded text-center">
+                <div class="feature">
                     <h3>Comparativa de Escenarios</h3>
                     <p>Compara diferentes escenarios de salud para tomar decisiones informadas sobre tu bienestar. Puedes ver cómo cambiaría tu vida en el futuro.</p>
                 </div>
             </div>
             <!-- Característica 4 -->
             <div class="col-md-4">
-                <div class="feature p-4 mb-4 bg-white rounded text-center">
+                <div class="feature">
                     <h3>Generación de Recomendaciones</h3>
                     <p>Basándonos en los resultados del análisis, te ofrecemos recomendaciones específicas para mejorar tu salud. Desde cambios en la dieta hasta rutinas de ejercicio.</p>
                 </div>
             </div>
             <!-- Característica 5 -->
             <div class="col-md-4">
-                <div class="feature p-4 mb-4 bg-white rounded text-center">
+                <div class="feature">
                     <h3>Asistente Virtual Interactivo</h3>
                     <p>Nuestro asistente virtual te guía a través de tus resultados, responde tus preguntas y te brinda apoyo en tu viaje hacia un mejor bienestar.</p>
                 </div>
             </div>
             <!-- Característica 6 -->
             <div class="col-md-4">
-                <div class="feature p-4 mb-4 bg-white rounded text-center">
+                <div class="feature">
                     <h3>Seguimiento Personalizado</h3>
                     <p>Te ayudamos a llevar un seguimiento de tu progreso y a establecer metas de salud personalizadas. Controla tu estado de salud de manera efectiva.</p>
                 </div>
@@ -50,5 +50,6 @@
         </div>
     </div>
 </section>
+
 
 <?php include 'footer.php'; ?>
