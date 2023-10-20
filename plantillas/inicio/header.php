e79d4821481fe409db58bed0ca3bfc0f7c786d18
Oscar Leal <toshiro120000@gmail.com>
2023-10-19 21:31:38 -0300

    a

--- plantillas/inicio/header.php
+++ plantillas/inicio/header.php
@@ -28,6 +28,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/capston/plantillas/evaluador_de_salud/evaluador.php">Evaluador(test)</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/capston/plantillas/evaluador_de_salud/evaluador_home.php">Evaluador home(test(test))</a>
+                </li>
             </ul>
             <ul class="navbar-nav">
                 <li class="nav-item dropdown">
