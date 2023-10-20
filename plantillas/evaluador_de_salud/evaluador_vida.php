818df2094b11ae84c778a7a634c54115262c0f66
Jonathan Bustamante <82405395+hasek2@users.noreply.github.com>
2023-10-19 22:16:46 -0300

    k

--- plantillas/evaluador_de_salud/evaluador_vida.php
+++ plantillas/evaluador_de_salud/evaluador_vida.php
@@ -4,76 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hábitos de Vida</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f0f0f0;
-        }
-
-        header {
-            background-color: #333;
-            color: white;
-            text-align: center;
-            padding: 1em;
-        }
-
-        .container {
-            max-width: 800px;
-            margin: 0 auto;
-            padding: 20px;
-            background-color: white;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            border-radius: 5px;
-            margin-top: 20px;
-        }
-
-        h1 {
-            text-align: center;
-        }
-
-        .question {
-            margin-bottom: 20px;
-        }
-
-        label {
-            font-weight: bold;
-        }
-
-        select {
-            width: 100%;
-            padding: 10px;
-            margin-top: 5px;
-            margin-bottom: 10px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-        }
-
-        .btn {
-            display: inline-block;
-            background-color: #333;
-            color: white;
-            padding: 10px 20px;
-            text-decoration: none;
-            border-radius: 5px;
-        }
-
-        .btn:hover {
-            background-color: #555;
-        }
-    </style>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <header>
+    <header class="bg-dark text-white text-center py-4">
         <h1>Hábitos de Vida</h1>
     </header>
 
-    <div class="container">
+    <div class="container my-4">
         <div class="question">
             <h2>Nivel de Actividad Física Actual</h2>
             <label for="activity-level">Selecciona tu nivel de actividad:</label>
-            <select id="activity-level">
+            <select class="form-control" id="activity-level">
                 <option value="sedentary">Sedentario</option>
                 <option value="lightly-active">Poco Activo</option>
                 <option value="moderately-active">Moderadamente Activo</option>
@@ -84,7 +26,7 @@
         <div class="question">
             <h2>Consumo de Tabaco</h2>
             <label for="smoking-habit">¿Fumas tabaco?</label>
-            <select id="smoking-habit">
+            <select class="form-control" id="smoking-habit">
                 <option value="regularly">Regularmente</option>
                 <option value="occasionally">Ocasionalmente</option>
                 <option value="never">Nunca</option>
@@ -94,7 +36,7 @@
         <div class="question">
             <h2>Consumo de Alcohol</h2>
             <label for="alcohol-habit">¿Consumes alcohol?</label>
-            <select id="alcohol-habit">
+            <select class="form-control" id="alcohol-habit">
                 <option value="regularly">Regularmente</option>
                 <option value="occasionally">Ocasionalmente</option>
                 <option value="never">Nunca</option>
@@ -104,7 +46,7 @@
         <div class="question">
             <h2>Hábitos Alimenticios</h2>
             <label for="eating-habits">Selecciona tus hábitos alimenticios:</label>
-            <select id="eating-habits">
+            <select class="form-control" id="eating-habits">
                 <option value="balanced-diet">Dieta Balanceada</option>
                 <option value="vegetarian">Vegetariano</option>
                 <option value="vegan">Vegano</option>
@@ -113,7 +55,7 @@
             </select>
         </div>
 
-        <a href="#" class="btn">Guardar</a>
+        <button class="btn btn-primary">Guardar</button>
     </div>
 </body>
 </html>
