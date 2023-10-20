24ff62a8c5b5d5c0f0ac92a8629cc9f0f1f537e6
Jonathan Bustamante <82405395+hasek2@users.noreply.github.com>
2023-10-19 21:24:16 -0300

    diseño

--- plantillas/inicio/footer.php
+++ plantillas/inicio/footer.php
@@ -1,11 +1,21 @@
-    <footer class="text-center py-4">
-        <p>&copy; <?php echo date("Y"); ?> Evaluador de Salud Personalizado</p>
-    </footer>
-    
-    
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<footer class="bg-body-tertiary py-4 text-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p class="mb-0">&copy; <?php echo date("Y"); ?> Evaluador de Salud Personalizado</p>
+            </div>
+            <div class="col-md-6">
+                <div class="text-end">
+                    <!-- Agregamos enlaces de redes sociales o información adicional si es necesario -->
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
 </html>
