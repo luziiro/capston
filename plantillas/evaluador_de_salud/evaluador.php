c2f9486fc5f5310828398a839cb7147e2388db47
Jonathan Bustamante <82405395+hasek2@users.noreply.github.com>
2023-11-14 22:57:12 -0300

    ia

--- plantillas/evaluador_de_salud/evaluador.php
+++ plantillas/evaluador_de_salud/evaluador.php
@@ -1,28 +1,61 @@
 <?php
+// Iniciar el manejo de errores y mostrar errores
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
+// Incluir el archivo de cabecera
 include '../inicio/header.php';
 
-// Función para obtener la respuesta de la IA (debes implementar esta función)
+// Función para enviar preguntas a la API de OpenAI
 function obtenerRespuestaIA($altura, $peso, $edad) {
-    // Aquí deberías implementar la lógica para generar recomendaciones de dieta y ejercicio
-    // basadas en los datos del usuario (altura, peso, edad).
-    
-    // Por ahora, simplemente devolvemos un mensaje de ejemplo.
-    $dieta = "Dieta recomendada: Consume una dieta equilibrada con frutas, verduras, proteínas magras y carbohidratos saludables.";
-    $rutinaEjercicio = "Rutina de ejercicio recomendada: Realiza al menos 30 minutos de ejercicio cardiovascular moderado, como caminar o nadar, 5 días a la semana. Además, incorpora ejercicios de fuerza para fortalecer tus músculos.";
-    
-    return "LA RECOMENDACION ES:\n\n$dieta\n\n$rutinaEjercicio";
+    // Crear una pregunta detallada para la IA (ajusta esta pregunta según tus necesidades)
+    $pregunta = "Basado en una altura de {$altura} metros, un peso de {$peso} kg y una edad de {$edad} años, ¿qué dieta y plan de vida recomiendas para mejorar la salud de esta persona?";
+
+    $data = [
+        'prompt' => $pregunta,
+        'max_tokens' => 150  // Ajustar el número de tokens para mantener la respuesta dentro del límite de 750 caracteres
+    ];
+
+    $api_key = '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'; // Reemplaza esto con tu clave API real
+
+    $ch = curl_init('https://api.openai.com/v1/engines/davinci/completions');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        'Authorization: Bearer ' . $api_key
+    ]);
+
+    $response = curl_exec($ch);
+    if (curl_errno($ch)) {
+        return 'Error en cURL: ' . curl_error($ch);
+    }
+    if (!$response) {
+        return 'Error en la solicitud a la API de OpenAI.';
+    }
+    curl_close($ch);
+    $response_data = json_decode($response, true);
+
+    if (!isset($response_data['choices'])) {
+        return 'Error: La respuesta de la API no contiene la clave "choices".';
+    }
+
+    return $response_data['choices'][0]['text'] ?? 'Error: Respuesta inesperada de la API.';
 }
 
-$recomendacion = "";
+$respuestaIA = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $altura = $_POST['altura'] ?? '';
-    $peso = $_POST['peso'] ?? '';
-    $edad = $_POST['edad'] ?? '';
+    // Asegurar que todos los campos estén presentes
+    if (isset($_POST['altura'], $_POST['peso'], $_POST['edad'])) {
+        $altura = $_POST['altura'];
+        $peso = $_POST['peso'];
+        $edad = $_POST['edad'];
 
-    // Llama a la función obtenerRespuestaIA con los datos del usuario
-    $recomendacion = obtenerRespuestaIA($altura, $peso, $edad);
+        $respuestaIA = obtenerRespuestaIA($altura, $peso, $edad);
+    } else {
+        $respuestaIA = "Por favor, completa todos los campos del formulario.";
+    }
 }
 ?>
 
@@ -30,78 +63,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Chat de Salud con IA</title>
-    <!-- Agrega la siguiente línea para cargar Bootstrap y jQuery -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    
-    <style>
-        /* Estilo personalizado para el modal */
-        .custom-modal {
-            max-width: 80%; /* Ancho máximo personalizado */
-            margin: 0 auto; /* Centrar horizontalmente */
-        }
-    </style>
+    <title>Consulta de Salud con IA</title>
+    <!-- Resto de tu código HTML para el head -->
 </head>
 <body>
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <h1 class="text-center">Chat de Salud con IA</h1>
-            <p class="text-center">Introduce tus datos de salud y recibe recomendaciones:</p>
-
-            <form method="post">
-                <div class="form-group">
-                    <label>Altura en metros:</label>
-                    <input type="number" class="form-control" name="altura" placeholder="Ejemplo: 1.75" step="0.01" required>
-                </div>
-                <div class="form-group">
-                    <label>Peso en kilogramos:</label>
-                    <input type="number" class="form-control" name="peso" placeholder="Ejemplo: 70" step="0.01" required>
-                </div>
-                <div class="form-group">
-                    <label>Edad:</label>
-                    <input type="number" class="form-control" name="edad" placeholder="Ejemplo: 30" required>
-                </div>
-                <!-- Puedes añadir más campos aquí según sea necesario -->
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Enviar</button>
-                </div>
-            </form>
+    <h1 class="text-center">Consulta de Salud con IA</h1>
+    <form method="post">
+        <div class="form-group">
+            <label>Altura en metros:</label>
+            <input type="number" class="form-control" name="altura" placeholder="Ejemplo: 1.75" step="0.01" required>
         </div>
-    </div>
-</div>
-
-<!-- Modal para mostrar la respuesta de la IA (modal-lg personalizado) -->
-<div class="modal fade" id="respuestaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog custom-modal" role="document"> <!-- Agrega la clase custom-modal aquí -->
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Respuesta de la IA</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <!-- Aquí se mostrará la respuesta de la IA después de enviar el formulario -->
-                <pre><?php echo $recomendacion; ?></pre>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-            </div>
+        <div class="form-group">
+            <label>Peso en kilogramos:</label>
+            <input type="number" class="form-control" name="peso" placeholder="Ejemplo: 70" step="0.01" required>
         </div>
-    </div>
+        <div class="form-group">
+            <label>Edad:</label>
+            <input type="number" class="form-control" name="edad" placeholder="Ejemplo: 30" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Consultar</button>
+    </form>
+    <?php if (!empty($respuestaIA)): ?>
+        <div class="alert alert-info mt-4">
+            <strong>Respuesta de IA:</strong> <br>
+            <?php echo htmlspecialchars($respuestaIA); ?>
+        </div>
+    <?php endif; ?>
 </div>
-
-<?php
-// Verifica si hay una respuesta de la IA y muestra el modal si es necesario
-if (!empty($recomendacion)) {
-    echo '<script>$("#respuestaModal").modal("show");</script>';
-}
-
-include '../inicio/footer.php';
-?>
+<!-- Resto de tu código HTML para el footer -->
+<?php include '../inicio/footer.php'; ?>
 </body>
 </html>
