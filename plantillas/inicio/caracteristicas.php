a8bc250a9405051a338317315074ec9f6b92a66c
Jonathan Bustamante <82405395+hasek2@users.noreply.github.com>
2023-11-20 23:50:46 -0300

    k

--- plantillas/inicio/caracteristicas.php
+++ plantillas/inicio/caracteristicas.php
@@ -7,6 +7,7 @@
             <!-- Característica 1 -->
             <div class="col-md-4">
                 <div class="feature p-4 mb-4 bg-white rounded text-center">
+                    <img src="../../img/analisisdetallado.webp" alt="Análisis Detallado" class="img-fluid mb-3">
                     <h3>Análisis Detallado</h3>
                     <p>Utilizamos algoritmos avanzados para evaluar tu estado de salud actual y predecir posibles riesgos. Nuestro análisis incluye datos biométricos, hábitos de vida y más.</p>
                 </div>
@@ -14,6 +15,7 @@
             <!-- Característica 2 -->
             <div class="col-md-4">
                 <div class="feature p-4 mb-4 bg-white rounded text-center">
+                    <img src="../../img/visualizacion.webp" alt="Visualización Interactiva" class="img-fluid mb-3">
                     <h3>Visualización Interactiva</h3>
                     <p>Explora visualmente cómo podrías lucir y sentirte en el futuro con cambios en tus hábitos. Nuestras visualizaciones te muestran aspectos físicos y emocionales.</p>
                 </div>
@@ -21,6 +23,7 @@
             <!-- Característica 3 -->
             <div class="col-md-4">
                 <div class="feature p-4 mb-4 bg-white rounded text-center">
+                    <img src="../../img/comparacion.webp" alt="Comparativa de Escenarios" class="img-fluid mb-3">
                     <h3>Comparativa de Escenarios</h3>
                     <p>Compara diferentes escenarios de salud para tomar decisiones informadas sobre tu bienestar. Puedes ver cómo cambiaría tu vida en el futuro.</p>
                 </div>
@@ -28,6 +31,7 @@
             <!-- Característica 4 -->
             <div class="col-md-4">
                 <div class="feature p-4 mb-4 bg-white rounded text-center">
+                    <img src="../../img/generacion.webp" alt="Generación de Recomendaciones" class="img-fluid mb-3">
                     <h3>Generación de Recomendaciones</h3>
                     <p>Basándonos en los resultados del análisis, te ofrecemos recomendaciones específicas para mejorar tu salud. Desde cambios en la dieta hasta rutinas de ejercicio.</p>
                 </div>
@@ -35,6 +39,7 @@
             <!-- Característica 5 -->
             <div class="col-md-4">
                 <div class="feature p-4 mb-4 bg-white rounded text-center">
+                    <img src="../../img/asistente virtual.webp" alt="Asistente Virtual Interactivo" class="img-fluid mb-3">
                     <h3>Asistente Virtual Interactivo</h3>
                     <p>Nuestro asistente virtual te guía a través de tus resultados, responde tus preguntas y te brinda apoyo en tu viaje hacia un mejor bienestar.</p>
                 </div>
@@ -42,11 +47,11 @@
             <!-- Característica 6 -->
             <div class="col-md-4">
                 <div class="feature p-4 mb-4 bg-white rounded text-center">
+                    <img src="../../img/seguimiento.webp" alt="Seguimiento Personalizado" class="img-fluid mb-3">
                     <h3>Seguimiento Personalizado</h3>
                     <p>Te ayudamos a llevar un seguimiento de tu progreso y a establecer metas de salud personalizadas. Controla tu estado de salud de manera efectiva.</p>
                 </div>
             </div>
-            <!-- Agregar más características según sea necesario -->
         </div>
     </div>
 </section>
