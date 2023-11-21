1d99c91f5c855c3613960363517ad86264fff864
Oscar Leal <toshiro120000@gmail.com>
2023-11-21 00:59:37 -0300

    a

--- plantillas/inicio/index.php
+++ plantillas/inicio/index.php
@@ -4,52 +4,52 @@ include 'header.php';
 
     <br><br>
 
-    <div class="container">
+    <div class="container mb-4">
         <div class="row">
-            <div class="col-md-6">
-                <img src="../../img/medicina.png" alt="Descripción de la imagen" class="img-fluid">
+            <div style="text-align: center;" class="col-md-6">
+                <img style="max-width: 70%;" src="../../img/health-future.png" alt="Descripción de la imagen" class="img-future">
             </div>
-            <div class="col-md-6">
-                <p>Bienvenido a nuestro Evaluador de Salud, 
+            <div style="position: relative; top: 60px;" class="col-md-6">
+                <h1 class="h1-home">Health Future: Tu Compañero en el Camino hacia el Bienestar Integral</h1>
+                <p class="p-home">Bienvenido a nuestro Evaluador de Salud, 
                 tu compañero personal en el viaje hacia un bienestar integral. 
                 Con solo unos clics, podrás obtener una visión clara de tu estado de salud actual y recibir recomendaciones 
                 personalizadas para mejorar tu calidad de vida.</p>
+                <a href="/capston/plantillas/registro/registro.php" class="btn-eva">Regístrate Ahora</a>
             </div>
         </div>
     </div>
 
+    <style>
+        .img-ancha img {
+            width: 100%; 
+            height: 100%;
+            display: block; 
+        }
+
+        .img-ancha {
+            height: 70%; 
+        }
+    </style>
+
+    <div class="img-ancha mb-4">
+        <img src="../../img/medicina.png" alt="">
+    </div>
 
     <div class="content">
-        <header class="text-center py-5">
-            <div class="container">
-                <?php
-                if (isset($_SESSION['usuario_logueado']) && $_SESSION['usuario_logueado']) {
-                    echo '<h1 class="display-4 cambio-color">Health Future</h1>';
-                    echo '<p class="lead">Descubre cómo tus elecciones de hoy pueden influir en tu bienestar futuro.</p>';
-                    echo '<a href="/capston/plantillas/evaluador_de_salud/evaluador.php" class="btn btn-primary btn-lg">Prueba nuestro evaluador</a>';
-                } else {
-                    echo '<h1 class="display-4 cambio-color">Health Future</h1>';
-                    echo '<p class="lead">Descubre cómo tus elecciones de hoy pueden influir en tu bienestar futuro.</p>';
-                    echo '<a href="/capston/plantillas/registro/registro.php" class="btn btn-primary btn-lg">Regístrate Ahora</a>';
-                }
-                ?>
-            </div>
-        </header>
-        <section class="quienes-somos">
-            <div class="container">
-                <h1>Quiénes Somos</h1>
-                <p>
-                    Somos Health Future, una plataforma dedicada a mejorar la salud y el bienestar de las personas mediante la aplicación de análisis de datos avanzados y visualización interactiva. Nuestra misión es proporcionar a los usuarios una evaluación personalizada de su salud actual y futura, inspirándolos a tomar decisiones informadas para un estilo de vida más saludable.
-                </p>
-                <p>
-                    En Health Future, utilizamos algoritmos de análisis de datos y modelos predictivos para ofrecer una representación visual única de cómo tus elecciones de salud pueden influir en tu vida a largo plazo. Nuestra plataforma te brinda la oportunidad de explorar diferentes escenarios, establecer metas de salud personalizadas y recibir recomendaciones específicas para tu bienestar.
-                </p>
-                <p>
-                    Nuestro equipo está compuesto por expertos en desarrollo de software, análisis de datos y diseño de experiencia de usuario. Trabajamos incansablemente para brindarte una herramienta poderosa y fácil de usar que te ayude a cuidar tu salud de manera efectiva y motivadora.
-                </p>
-                        </div>
-            </div>
+        <div class="container">
+            <h1>Quiénes Somos</h1>
+            <p>
+                Somos Health Future, una plataforma dedicada a mejorar la salud y el bienestar de las personas mediante la aplicación de análisis de datos avanzados y visualización interactiva. Nuestra misión es proporcionar a los usuarios una evaluación personalizada de su salud actual y futura, inspirándolos a tomar decisiones informadas para un estilo de vida más saludable.
+            </p>
+            <p>
+                En Health Future, utilizamos algoritmos de análisis de datos y modelos predictivos para ofrecer una representación visual única de cómo tus elecciones de salud pueden influir en tu vida a largo plazo. Nuestra plataforma te brinda la oportunidad de explorar diferentes escenarios, establecer metas de salud personalizadas y recibir recomendaciones específicas para tu bienestar.
+            </p>
+            <p>
+                Nuestro equipo está compuesto por expertos en desarrollo de software, análisis de datos y diseño de experiencia de usuario. Trabajamos incansablemente para brindarte una herramienta poderosa y fácil de usar que te ayude a cuidar tu salud de manera efectiva y motivadora.
+            </p>
         </div>
+    </div>
 
 <?php
 include 'footer.php';
